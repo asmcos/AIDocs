17ada605038cf76a9b05f50e8928aaef8314b676
asmcos <asmcos@gmail.com>
2022-01-15 19:48:28 +0800

    增加吴恩达老师课程中的数据

---
+++ examples/dl_lession_week2.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+import os
+
+dir_name = os.path.dirname(__file__)
+#吴恩达老师的猫,通过网上下载
+#有209张图片作为训练集，50张图片作为测试集，图片中有的是猫的图片，有的不是。
+#每张图片的像素大小为64*64
+def load_dataset():
+    with h5py.File(dir_name+'/train_catvnoncat.h5', "r") as train_dataset:
+        train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+        train_set_y_orig = np.array(train_dataset["train_set_y"][:])
+
+    with h5py.File(dir_name+'/test_catvnoncat.h5', "r") as test_dataset:
+        test_set_x_orig = np.array(test_dataset["test_set_x"][:])
+        test_set_y_orig = np.array(test_dataset["test_set_y"][:])
+        classes = np.array(test_dataset["list_classes"][:])
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+# Example of a picture
+index = 25
+plt.imshow(train_set_x_orig[index])
+print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+
