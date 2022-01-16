f13e415ff26c7975d61170836ae09e95b6b6a601
asmcos <asmcos@gmail.com>
2022-01-16 21:42:47 +0800

    修改db

--- examples/server/db.py
+++ examples/server/db.py
@@ -2,14 +2,26 @@ from sqlalchemy import create_engine,Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 import sys
 from sqlalchemy.orm import sessionmaker
+# 1.3.0
 
 Base = declarative_base()
+
 class Image(Base):
     __tablename__ = 'img_info'
 
     id = Column(Integer, primary_key=True)
     imagename = Column(String(255))
     imagelabel = Column(Integer)
+
+class predImage(Base):
+    __tablename__ = 'predimg_info'
+
+    id =        Column(Integer, primary_key=True)
+    imagename = Column(String(255))
+    imagelabel= Column(Integer)
+    pred =      Column(Integer)
+
+
 
 engine = create_engine('sqlite:///'+sys.path[0]+'/img.db')
 
@@ -18,10 +30,37 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 def insert(filename,label):
-    img = Image(filename, label)
+    img = predImage(imagename=filename, pred=pred)
     session.add(img)
     session.commit()
 
+def update(imgid,y):
+    img = session.query(predImage).get(imgid)
+    img.imagelabel = y
+    session.commit()
+
+def deletepred(predid):
+    preimg = session.query(predImage).get(imgid)
+    session.delete(preimg)
+    session.commit() 
+
+def get_all_pred():
+    imgs = session.query(preImage)
+    return imgs
+
+def makeImagefrompred(predId,label):
+    predimg = session.query(predImage).get(imgid)
+    if predimg:
+        img = session.query(Image).filter_by(imagename=predimg.imagename).first()
+        if img:
+            img.imagelabel=label
+        else:
+            img = Image({'imagename':predimg.imagename,'imagelabel':label})
+            session.add(img)
+        session.commit()
+    return "ok"
+
+            
 def get_all():
     imgs = session.query(Image)
     for row in imgs: 
