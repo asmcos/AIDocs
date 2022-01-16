d0bf9194d60407d4baada94c99ad1d89f22ba291
asmcos <asmcos@gmail.com>
2022-01-16 16:13:06 +0800

    识别图片

--- examples/server/server.py
+++ examples/server/server.py
@@ -1,19 +1,18 @@
 from flask import Flask
 from flask import render_template,jsonify,request
 import random,string
-import os
+import os,sys
 app = Flask(__name__,static_folder="./uploads")
 
-
-if not os.path.exists('uploads'):
-    os.makedirs('uploads')
+if not os.path.exists(sys.path[0]+'/uploads'):
+    os.makedirs(sys.path[0]+'/uploads')
 
 @app.route("/uploads",methods=['GET', 'POST'])
 def uploadsfile():
     if request.method == 'POST':
         f = request.files['image']
         randval = "".join(random.sample(string.digits+string.ascii_letters,8))
-        f.save('uploads/'+randval+f.filename)
+        f.save(sys.path[0]+'/uploads/'+randval+f.filename)
 
     return jsonify({"errno": 0,\
              "data": ["/uploads/"+randval+f.filename,]
