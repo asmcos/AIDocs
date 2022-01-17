4f86f5652e944d4e88cf067d95c8be649b820a16
jiasheng he <jiashenghe@jiashengtekiMacBook-Air.local>
2022-01-17 12:03:38 +0800

    网络识别模型

--- examples/server/server.py
+++ examples/server/server.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask import render_template,jsonify,request
 import random,string
 import os,sys
-from db import insert, get_all 
+from db import insert, get_all_pred,makeImagefrompred 
+
+from dl_lession_week2 import pred_cat
 
 app = Flask(__name__,static_folder="./uploads")
+
+app.config['DEBUG'] = True
 
 if not os.path.exists(sys.path[0]+'/uploads'):
     os.makedirs(sys.path[0]+'/uploads')
@@ -19,6 +23,24 @@ def uploadsfile():
     return jsonify({"errno": 0,\
              "data": ["/uploads/"+randval+f.filename,]
             })
+
+
+@app.route("/pred",methods=['GET', 'POST'])
+def pred():
+    if request.method == 'POST':
+        filename = request.form.get('filename')
+        print(filename)
+        print(pred_cat(sys.path[0]+filename))
+        #insert(filename,pred)
+    return jsonify({"pred":1})
+
+@app.route("/accept",methods=['GET', 'POST'])
+def accept():
+    if request.method == 'POST':
+        pass
+        #makeImagefrompred(predid,label)
+    return jsonify({"success":1})
+
 
 @app.route('/')
 def index():
